773993cc50e3fd2b2bb248a5092e1398d435229c
Ahmed Farghaly <afarghaly@gmail.com>
2016-07-18 20:07:09 -0700

    Empty Xcode Project

---
+++ dev/SortanimTests/SortanimTests.swift
@@ -0,0 +1,39 @@
+//
+//  SortanimTests.swift
+//  SortanimTests
+//
+//  Created by Ahmed Farghaly on 7/18/16.
+//  Copyright © 2016 Ahmed Farghaly. All rights reserved.
+//
+
+import XCTest
+@testable import Sortanim
+
+class SortanimTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssert("Initial commit" == "Initial commit", "Initial commit is NOT EQUAL TO Initial Commit")
+    }
+    
+    /*
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    */
+}
